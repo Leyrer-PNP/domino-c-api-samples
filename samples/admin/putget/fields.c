9b758d7bd88755efef217f925d58a6d0e34a3152
Shaik Nayab Rasool <shaiknayabrasool.s@prod.hclpnp.com>
2023-05-24 23:29:14 +0530

    DNEXT-14842 Add License header for All CAPI samples

--- samples/admin/putget/fields.c
+++ samples/admin/putget/fields.c
@@ -1,5 +1,19 @@
-/* DESCRIPTION
-
+/*
+* Copyright HCL Technologies 1996, 2023.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* DESCRIPTION
 This source file contains low-level routines that read and write fields
 in Domino and Notes documents. These functions are used by the GETNOTE 
 and PUTNOTE programs.
